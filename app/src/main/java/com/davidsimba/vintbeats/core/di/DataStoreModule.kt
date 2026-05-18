20ae750144b66960253a60198d5aa74a951abda0
David Simba <davidsimba200@gmail.com>
2026-05-18 12:56:56 -0500

    feat: add DI data store module

---
+++ app/src/main/java/com/davidsimba/vintbeats/core/di/DataStoreModule.kt
@@ -0,0 +1,31 @@
+package com.davidsimba.vintbeats.core.di
+
+import android.content.Context
+import com.davidsimba.vintbeats.core.datastore.TokenStorage
+import com.davidsimba.vintbeats.core.datastore.TokenStorageImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataStoreModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindTokenStorage(
+        impl: TokenStorageImpl
+    ): TokenStorage
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideApplicationContext(
+            @ApplicationContext context: Context
+        ): Context = context
+    }
+}
