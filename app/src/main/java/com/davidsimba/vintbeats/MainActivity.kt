72eb7eef03f002e8589a88d2d9bc11e68f801ae8
David Simba <davidsimba200@gmail.com>
2026-05-30 19:52:50 -0500

    feat: implement splash screen

--- app/src/main/java/com/davidsimba/vintbeats/MainActivity.kt
+++ app/src/main/java/com/davidsimba/vintbeats/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,6 +32,7 @@ class MainActivity : ComponentActivity() {
     private var pendingNavRoute by mutableStateOf<String?>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
